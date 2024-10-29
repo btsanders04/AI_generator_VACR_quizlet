22b8a10703a2ecd1a6c3454cd49e6cec684681b7
Brandon <btsanders04@gmail.com>
2024-10-28 20:32:00 -0400

    format

--- app/components/AircraftInfo.tsx
+++ app/components/AircraftInfo.tsx
@@ -27,27 +27,29 @@ const AircraftInfo = ({ aircraft }: AircraftInfoProps) => {
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <div className="max-w-2xl w-full bg-white rounded-lg shadow-md p-6">
         <h1 className="text-3xl font-bold mb-6 text-center text-black">{aircraft}</h1>
-            <ImageCarousel
-                images={aircraftData?.imageUrls || []}
-              />
-        
+        <ImageCarousel images={aircraftData?.imageUrls || []} />
+
         <h2 className="text-1xl text-center font-bold text-left text-black">General Data</h2>
         <div className="text-gray-600">
-         {aircraftData?.generalData.map(item => (
-          <p
-          key={`general-${item.key}`}
-          className="flex items-center gap-2 p-3 bg-gray-100  group transition-colors duration-200"
-          >{item.key}: {item.value}</p>
-         ))}
+          {aircraftData?.generalData.map(item => (
+            <p
+              key={`general-${item.key}`}
+              className="flex items-center gap-2 p-3 bg-gray-100  group transition-colors duration-200"
+            >
+              {item.key}: {item.value}
+            </p>
+          ))}
         </div>
         <h2 className="text-1xl text-center  font-bold text-left text-black">Weft Description</h2>
         <div className=" text-gray-600">
-         {aircraftData?.weftDescription.map(item => (
-          <p
-          key={`weft-${item.key}`}
-          className="flex items-center gap-2 p-3 bg-gray-100 group  transition-colors duration-200"
-          >{item.key}: {item.value}</p>
-         ))}
+          {aircraftData?.weftDescription.map(item => (
+            <p
+              key={`weft-${item.key}`}
+              className="flex items-center gap-2 p-3 bg-gray-100 group  transition-colors duration-200"
+            >
+              {item.key}: {item.value}
+            </p>
+          ))}
         </div>
       </div>
     </div>
